b56d6bc13410b01c8bec1029df9ef1feb1098e99
leog <leog91@gmail.com>
2023-05-14 04:24:07 +0200

    mp3 font tweaks

--- components/Mp3Player.tsx
+++ components/Mp3Player.tsx
@@ -7,9 +7,11 @@ export const Mp3Player = () => {
 
   return (
     <div className="flex flex-col  bg-black py-1 border-2 border-red-600 px-8  rounded-full items-center">
-      <div className="p-1">mp3Player</div>
+      <div className="p-1 font-semibold text-sm">DIGITAL MP3 PLAYER</div>
       <div className="flex  items-center ">
-        <div className="p-3">MP3</div>
+        <div className="p-3  font-bold">
+          <div>MP3</div>
+        </div>
         <div className="bg-teal-600 text-black text-center gap-2 p-2 flex ">
           <div className="relative flex flex-col w-7 h-[50px] border-2 border-black rounded-md">
             <div className="border-b-2 relative bg-teal-600 text-lg bg border-black">
@@ -44,9 +46,9 @@ export const Mp3Player = () => {
             <div className="text-xs relative bg-teal-600">SYS</div>
           </div>
         </div>
-        <div className="p-3">USB</div>
+        <div className="p-3 font-bold">USB</div>
       </div>
-      <div className="p-1">MP3/WMA/REC</div>
+      <div className="p-1 font-semibold text-sm">MP3/WMA/REC</div>
     </div>
   );
 };
