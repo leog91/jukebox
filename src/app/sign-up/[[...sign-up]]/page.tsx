de6ada5f91bdfae331305be2f082aab6467c3db4
leog <leog91@gmail.com>
2025-01-06 19:19:00 +0000

    added fallback redirect url on log in

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="min-h-screen justify-center bg-black flex flex-col m-auto max-w-2xl  items-center ">
-      <SignUp />
+      <SignUp fallbackRedirectUrl="/pick" />
     </div>
   );
 }
