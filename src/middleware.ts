b477c2e181e5167e27f2b2655ad5ad9d2d50119e
leog <leog91@gmail.com>
2025-01-09 20:40:16 +0000

    add protected route for adding an artist

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 
 
-const isProtectedRoute = createRouteMatcher(['/pick/artist(.*)'])
+const isProtectedRoute = createRouteMatcher(['/pick/artist(.*)',
+    "/pick/add"])
 
 
 export default clerkMiddleware(async (auth, req) => {
