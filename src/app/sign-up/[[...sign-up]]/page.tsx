104ab4d6178a36c43694e19727541709dcde22b4
leog <leog91@gmail.com>
2025-10-15 18:45:13 +0100

    Refactor authentication links: replace hardcoded login link with SignInLink and add SignUpLink component

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import { SignUp } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
+  const searchParams = useSearchParams();
+  const redirectUrl = searchParams.get("redirect_url") ?? "/";
+
   return (
     <div className="min-h-screen justify-center bg-black flex flex-col m-auto max-w-2xl  items-center ">
-      <SignUp fallbackRedirectUrl="/pick" />
+      <SignUp forceRedirectUrl={redirectUrl} />
     </div>
   );
 }
